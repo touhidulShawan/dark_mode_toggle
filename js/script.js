d0b47e2aec71eec7d46cc4309f192fba1f74bd17
Touhidul Shawan <touhidulshawan@gmail.com>
2020-01-14 21:39:00 +0600

    make some comment

--- js/script.js
+++ js/script.js
@@ -2,15 +2,19 @@ let darkmode = localStorage.getItem("darkmode");
 
 const darkModeToggler = document.getElementById("dark-mode-toggler");
 
+//  enable dark mode
 const enableDarkMode = () => {
   document.body.classList.add("darkmode");
   localStorage.setItem("darkmode", "enabled");
 };
 
+//  disable dark mode
 const disableDarkMode = () => {
   document.body.classList.remove("darkmode");
   localStorage.setItem("darkmode", null);
 };
+
+// check if already dark mode is enabled
 
 if (darkmode == "enabled") {
   enableDarkMode();
